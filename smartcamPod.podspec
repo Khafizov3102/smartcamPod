9bfee0227dfd64bc6ab44691bb9e58a84c227517
Khafizov <khafizov.den@yandex.ru>
2024-04-26 14:13:52 +0500

    init commit

---
+++ smartcamPod.podspec
@@ -0,0 +1,9 @@
+Pod::Spec.new do |s|
+  s.name         = 'smartcamPod'
+  s.version      = '1.0.0'
+  s.summary      = 'smartcamPod.'
+  s.homepage     = 'https://github.com/Khafizov3102/smartcamPod
+  s.license      = 'MIT'
+  s.author       = { 'Denis Khafizov' => 'khafizov.den@yandex.ru' }
+  s.source_files  = 'smartcamPod/Sources/**/*'
+end
---
+++ smartcamPod/smartcamPod.h
@@ -0,0 +1,18 @@
+//
+//  smartcamPod.h
+//  smartcamPod
+//
+//  Created by Денис Хафизов on 26.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for smartcamPod.
+FOUNDATION_EXPORT double smartcamPodVersionNumber;
+
+//! Project version string for smartcamPod.
+FOUNDATION_EXPORT const unsigned char smartcamPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <smartcamPod/PublicHeader.h>
+
+
